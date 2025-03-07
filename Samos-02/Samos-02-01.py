f6f84590a322682276e8879446f0576bc8165422
Oleksandr_Rudniev_PD14 <st9354750@stud.duikt.edu.ua>
2025-03-07 10:03:19 +0200

    Python Lab-02-final

--- Samos-02/Samos-02-01.py
+++ Samos-02/Samos-02-01.py
@@ -1,20 +1,46 @@
 import math
 
-
-def try_variable(var):
+def input_number_with_check():
+    input_string = input("Введіть число: ")
     try:
-        val = var
-    except NameError:
-        return None
-    return val
+        input_float = float(input_string)
+        if input_float == 0: return int(input_float)
+        elif input_float.is_integer(): return int(input_float)
+        else: return input_float
+    except ValueError:
+        print("неправильні дані!")
+        return input_number_with_check()
+
+def input_positive_number_with_check():
+    input_string = input("Введіть число: ")
+    try:
+        input_float = float(input_string)
+        if input_float > 0:
+            if input_float.is_integer():
+                return int(input_float)
+            else:
+                return input_float
+        else:
+            print("Число має бути більше 0!")
+            return input_positive_number_with_check()
+    except ValueError:
+        print("неправильні дані!")
+        return input_positive_number_with_check()
 
 def coordinates_2d_input():
-    x = int(input("Введіть х: "))
-    y = int(input("Введіть y: "))
+    print("Ввод х")
+    x = input_number_with_check()
+    print("Ввод y")
+    y = input_number_with_check()
     return x, y
 
 def print_2d_coordinates(x,y):
-    print(f"Координати точки в 2-хвимірному просторі - x = {x}, y = {y}")
+    if x == 0: x = int(x);
+    elif x % int(x) == 0: x = int(x)
+    if y == 0: y = int(y);
+    elif y % int(y) == 0: y = int(y)
+    print(f"Координати точки в 2-хвимірному просторі - x = {x}, y = {y},"
+          f"\nінший вигляд: А({x};{y})")
 
 def coordinates_2d():
     coordinates_exist = 0
@@ -23,19 +49,29 @@ def coordinates_2d():
         if action.lower() == 'i':
             x, y = coordinates_2d_input()
             coordinates_exist = 1
-        elif coordinates_exist == 1 and action.lower() == 'o':
+        if coordinates_exist == 1 and action.lower() == 'o':
             print_2d_coordinates(x,y)
-        elif action.lower() == 'b':
+        if action.lower() == 'b':
             break
 
 def coordinates_3d_input():
-    x = int(input("Введіть х: "))
-    y = int(input("Введіть y: "))
-    z = int(input("Введіть z: "))
+    print("Ввод х")
+    x = input_number_with_check()
+    print("Ввод y")
+    y = input_number_with_check()
+    print("Ввод z")
+    z = input_number_with_check()
     return x, y, z
 
 def print_3d_coordinates(x,y,z):
-    print(f"Координати точки в 3-хвимірному просторі - x = {x}, y = {y}, z = {z}")
+    if x == 0: x = int(x);
+    elif x % int(x) == 0: x = int(x)
+    if y == 0: y = int(y);
+    elif y % int(y) == 0: y = int(y)
+    if z == 0: z = int(z);
+    if z % int(z) == 0: z = int(z)
+    print(f"Координати точки в 3-хвимірному просторі - x = {x}, y = {y}, z = {z},"
+          f"\nінший вигляд: А({x};{y};{z})")
 
 def coordinates_3d():
     coordinates_exist = 0
@@ -50,8 +86,10 @@ def coordinates_3d():
             break
 
 def mathematical_expressions():
-    a = int(input("Введіть а: "))
-    b = int(input("Введіть b: "))
+    print("Ввод а")
+    a = input_number_with_check()
+    print("Ввод b")
+    b = input_number_with_check()
     formulae_1 = math.log(a ** 2 + b ** 2)
     formulae_2 = math.sin((a + b) ** (1 / 7))
     formulae_3 = (math.cos(a ** 12 + b ** 12)) ** 3
@@ -62,17 +100,17 @@ def mathematical_expressions():
     """)
 
 def triangle_perimeter():
-    first_side_input = input("Введіть першу сторону трикутника: ")
-    second_side_input = input("Введіть другу сторону трикутника: ")
-    third_side_input = input("Введіть третю сторону трикутника: ")
-    if first_side_input.isdigit() and second_side_input.isdigit() and third_side_input.isdigit():
-        first_side = int(first_side_input)
-        second_side = int(second_side_input)
-        third_side = int(third_side_input)
-        perimeter = first_side + second_side + third_side
+    print("Перша сторона трикутника")
+    side1 = input_positive_number_with_check()
+    print("Друга сторона трикутника")
+    side2 = input_positive_number_with_check()
+    print("Третя сторона трикутника")
+    side3 = input_positive_number_with_check()
+    if side1 < side2 + side3 and side2 < side1 + side3 and side3 < side1 + side2:
+        perimeter = side1 + side2 + side3
         print(f'Периметр трикутника = {perimeter}')
     else:
-        print("Некоректний ввод")
+        print("Такого трикутника не існує, перевірте правильність даних")
 
 def math_module():
     while True:
@@ -85,31 +123,109 @@ def math_module():
         tan - тангенс
         fact - факторіал
         exp - піднесення е у степінь
-        q - назад
-        """)
+        q - назад: """)
+
         if action.lower() == 's':
-            a = int(input("Введіть ціле число: "))
+            a = input_positive_number_with_check()
             print(f'\u221A{a} = {math.sqrt(a)}')
+
         if action.lower() == 'log':
-            base = int(input("Введіть основу логарифма: "))
-            a = int(input("Введіть логарифмоване число: "))
-            print(f'log(base {base}){a} = {math.log(a, base)}')
+            print("Основа логарифма")
+            base = input_positive_number_with_check()
+            if base == 1:
+                print("Основа на може бути 1!")
+                base = input_positive_number_with_check()
+            print("Аргумент логарифма")
+            a = input_positive_number_with_check()
+            res = math.log(a, base)
+            if res == 0: res = int(res)
+            print(f'log(base {base}){a} = {res}')
+
         if action.lower() == 'sin':
-            x = int(input("Введіть ціле число: "))
+            print("кут (радіани)")
+            x = input_number_with_check()
             print(f'sin({x}) = {math.sin(x)}')
+
         if action.lower() == 'cos':
-            x = int(input("Введіть ціле число: "))
+            print("кут (радіани)")
+            x = input_number_with_check()
             print(f'cos({x}) = {math.cos(x)}')
+
         if action.lower() == 'tan':
-            x = int(input("Введіть ціле число: "))
+            print("кут (радіани)")
+            x = input_number_with_check()
             print(f'tg({x}) = {math.tan(x)}')
+
         if action.lower() == 'fact':
-            x = int(input("Введіть ціле число: "))
-            print(f'{x}! = {math.factorial(x)}')
+            x = input_positive_number_with_check()
+            if x.is_integer(): print(f'{x}! = {math.factorial(x)}')
+            else:
+                print("Число має бути цілим!")
+
         if action.lower() == 'exp':
-            x = int(input("Введіть ступінь експоненти: "))
-            print(f'е^{x} = {math.exp(x)}')
+            print("ступінь експоненти")
+            x = input_number_with_check()
+            res = math.exp(x)
+            if res == 1: res = int(res)
+            print(f'е^{x} = {res}')
+
         if action.lower() == 'q': break
+
+def calculate_radius_from_circumference(circumference):
+    return circumference / (2 * math.pi)
+
+def calculate_area_from_radius(radius):
+    return math.pi * radius ** 2
+
+def circle_module():
+    while True:
+        action = input("""
+        Виберіть геометричну функцію:
+        r - обчислення радіуса за довжиною кола
+        a - обчислення площі круга за радіусом
+        ra - обчислення радіуса і площі за довжиною кола
+        q - вихід: """)
+
+        if action.lower() == 'r':
+            print("Довжина кола")
+            circumference = input_positive_number_with_check()
+            print(f"радіус = {calculate_radius_from_circumference(circumference)}")
+
+        if action.lower() == 'a':
+            print("радіус")
+            radius = input_positive_number_with_check()
+            print(f"площа круга = {calculate_area_from_radius(radius)}")
+
+        if action.lower() == 'ra':
+            print("Довжина кола")
+            circumference = input_positive_number_with_check()
+            radius = calculate_radius_from_circumference(circumference)
+            area = calculate_area_from_radius(radius)
+            print(f"радіус = {radius}, площа = {area}")
+
+        if action.lower() == 'q': break
+
+def format_module():
+    number = input_number_with_check()
+    number_dot_digits = int(input("введіть кількість знаків після коми: "))
+    res_rounded = round(number, number_dot_digits)
+    print(f'число = {res_rounded}')
+    print(f'scientific = {number:.2e}')
+
+def print_equation(a, b):
+    print(f'Рівняння: {a}x + {b} = 0')
+
+def equation_interface():
+    print("аргумент а")
+    a = input_number_with_check()
+    print("аргумент b")
+    b = input_number_with_check()
+    print_equation(a, b)
+
+def greetings():
+    name = input("Введіть ваше ім'я: ")
+    surname = input("Введіть ваше прізвище: ")
+    print(f'Привіт, {name} {surname}!')
 
 def menu():
     while True:
@@ -122,13 +238,18 @@ def menu():
         math - використання функцій модуля math
         g - робота з геометричними об'єктами
         f - форматування чисел та виведення даних
+        eq - рівняння
+        greet - привітання
         q - вихід: """)
         if action.lower() == '2d': coordinates_2d()
         if action.lower() == '3d': coordinates_3d()
         if action.lower() == 'm': mathematical_expressions()
         if action.lower() == 'p': triangle_perimeter()
         if action.lower() == 'math': math_module()
+        if action.lower() == 'g': circle_module()
+        if action.lower() == 'f': format_module()
+        if action.lower() == 'eq': equation_interface()
+        if action.lower() == 'greet': greetings()
         if action.lower() == 'q': break
-
 
 menu()
